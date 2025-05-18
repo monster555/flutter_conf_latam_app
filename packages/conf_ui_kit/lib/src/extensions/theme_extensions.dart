1e093fbbb444eaedd8c85a0f97a28d4f58a7e2bc
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-05-18 16:23:05 -0500

    📝 (conf_ui_kit.dart): add new 'gen.dart' and 'snackbar.dart' files to exports for easier access in other files ♻️ (theme_extensions.dart): refactor ThemeX extension to include new color-related properties and methods for better theme customization and readability

--- packages/conf_ui_kit/lib/src/extensions/theme_extensions.dart
+++ packages/conf_ui_kit/lib/src/extensions/theme_extensions.dart
@@ -1,4 +1,5 @@
 import 'package:conf_ui_kit/src/theme/app_assets.dart';
+import 'package:conf_ui_kit/src/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 extension ThemeX on BuildContext {
@@ -8,6 +9,32 @@ extension ThemeX on BuildContext {
 
   Color get scaffoldBackgroundColor => Theme.of(this).scaffoldBackgroundColor;
 
-  AppAssets get appAssets =>
-      Theme.of(this).extension<AppAssets>() ?? AppAssets.light;
+  AppAssets get appAssets {
+    return Theme.of(this).extension<AppAssets>() ?? AppAssets.light;
+  }
+
+  bool get dark => colorScheme.brightness == Brightness.dark;
+
+  TextStyle? get primaryButton {
+    final bodyMedium = textTheme.bodyMedium;
+    return switch (dark) {
+      true => bodyMedium?.copyWith(color: AppColors.lightText),
+      false => bodyMedium?.copyWith(color: AppColors.onPrimary),
+    };
+  }
+
+  TextStyle? get secondaryButton => switch (dark) {
+    true => textTheme.bodyMedium?.copyWith(color: AppColors.onPrimary),
+    false => null,
+  };
+
+  Color get primaryButtonColor => switch (dark) {
+    true => AppColors.onPrimary,
+    false => AppColors.onPrimaryDark,
+  };
+
+  Color get primaryButtonOverlayColor => switch (dark) {
+    true => AppColors.onPrimaryDark,
+    false => AppColors.onPrimary,
+  };
 }
