e7e8997f9bf6f7e3963a97b309697fb4e00f461f
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-05-18 16:21:14 -0500

    ✨ (app_buttons.dart): introduce new base button widget and social button widget to provide reusable button components for the UI kit 📝 (buttons.dart): add export statement for app_buttons.dart to make the new button widgets accessible from outside the buttons directory

---
+++ packages/conf_ui_kit/lib/src/widgets/buttons/app_buttons.dart
@@ -0,0 +1,76 @@
+import 'package:conf_ui_kit/conf_ui_kit.dart';
+import 'package:flutter/material.dart';
+
+class AppBaseButton extends StatelessWidget {
+  const AppBaseButton({
+    required this.child,
+    required this.color,
+    required this.onPressed,
+    super.key,
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+    this.side = BorderSide.none,
+    this.overlayColor,
+    this.padding = const EdgeInsets.all(UiConstants.spacing12),
+  });
+  final BorderRadiusGeometry borderRadius;
+  final Widget child;
+  final Color color;
+  final VoidCallback onPressed;
+  final BorderSide side;
+  final Color? overlayColor;
+  final EdgeInsetsGeometry padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        overlayColor: overlayColor,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        padding: padding,
+        shape: RoundedRectangleBorder(side: side, borderRadius: borderRadius),
+      ),
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+}
+
+class SocialButton extends StatelessWidget {
+  const SocialButton({
+    required this.color,
+    required this.icon,
+    required this.text,
+    required this.onPressed,
+    this.style,
+    super.key,
+    this.overlayColor,
+    this.side = BorderSide.none,
+  });
+  final VoidCallback onPressed;
+  final Color color;
+  final Color? overlayColor;
+  final Widget icon;
+  final String text;
+  final TextStyle? style;
+  final BorderSide side;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBaseButton(
+      color: color,
+      overlayColor: overlayColor,
+      onPressed: onPressed,
+      side: side,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          icon,
+          const SizedBox(width: UiConstants.spacing12),
+          Text(text, style: style),
+        ],
+      ),
+    );
+  }
+}
