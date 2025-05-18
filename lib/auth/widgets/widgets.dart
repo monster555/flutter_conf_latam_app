d3543f857757c6f16136c7c12f703e86fa8165dd
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-05-18 16:26:35 -0500

    ✨ (widgets.dart): introduce new widget files for authentication flow in the app

---
+++ lib/auth/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'auth_header.dart';
+export 'continue_as_guest.dart';
+export 'continue_with_apple.dart';
+export 'continue_with_google.dart';
+export 'or.dart';
+export 'terms_and_conditions.dart';
