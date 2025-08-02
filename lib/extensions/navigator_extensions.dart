1ea78a043fec4d2b22ff6e3fd98e851e919d1a09
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-02 12:48:25 -0500

    ♻️ (navigator_extensions.dart): refactor push method name from push to pushX for better clarity and to avoid potential conflicts with existing methods

--- lib/extensions/navigator_extensions.dart
+++ lib/extensions/navigator_extensions.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 extension NavigatorX on BuildContext {
   NavigatorState get _navigator => Navigator.of(this);
 
-  Future<T?> push<T>(Widget page) => _navigator.push(
+  Future<T?> pushX<T>(Widget page) => _navigator.push(
     MaterialPageRoute<T>(builder: (BuildContext context) => page),
   );
 }
