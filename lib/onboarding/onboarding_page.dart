e69cdaf503d2a8fdb5e360de7060a9907dc2e6cb
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-02 12:52:01 -0500

    ✨ (onboarding_page.dart): introduce a new OnboardingPage class to display onboarding screen in the app

---
+++ lib/onboarding/onboarding_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage._();
+
+  static Widget builder(BuildContext _, GoRouterState __) {
+    return const OnboardingPage._();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('OnboardingPage Screen')));
+  }
+}
