75038039561ae3f956bc42d6fefac1f347cf582a
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-02 12:49:45 -0500

    ✨ (sign_in_cubit.dart): Add support for different sign-in types and error handling in the SignInCubit 📝 (sign_in_state.dart): Add new states for loading, success, and error in the SignInState class

--- lib/auth/cubits/sign_in/sign_in_cubit.dart
+++ lib/auth/cubits/sign_in/sign_in_cubit.dart
@@ -1,6 +1,6 @@
-import 'dart:developer';
-
+import 'package:conf_auth_data_source/conf_auth_data_source.dart';
 import 'package:conf_auth_repository/conf_auth_repository.dart';
+import 'package:conf_shared_models/conf_shared_models.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'sign_in_state.dart';
@@ -13,11 +13,26 @@ class SignInCubit extends Cubit<SignInState> {
   final IConfAuthRepository _authRepository;
 
   Future<void> signIn(SignInType type) async {
-    final userResult = await switch (type) {
+    emit(const SignInLoadingState());
+    final result = await switch (type) {
       SignInType.apple => _authRepository.signInWithApple(),
       SignInType.google => _authRepository.signInWithGoogle(),
       SignInType.guest => _authRepository.signInAnonymously(),
     };
-    log('Sign In with ${type.name}...', name: 'SignInCubit');
+    final newState = switch (result) {
+      Success() => const SignInSuccessState(),
+      Failure(:final error) when error.aborter => const SingInInitialState(),
+      Failure(:final error) => SignInErrorState(error.fromException()),
+    };
+    emit(newState);
   }
+}
+
+extension on ConfAuthDataSourceException {
+  bool get aborter => this is UserAbortedSignInException;
+
+  SignInErrorType fromException() => switch (this) {
+    NoInternetException() => SignInErrorType.internet,
+    _ => SignInErrorType.generic,
+  };
 }
--- lib/auth/cubits/sign_in/sign_in_state.dart
+++ lib/auth/cubits/sign_in/sign_in_state.dart
@@ -7,3 +7,18 @@ sealed class SignInState {
 class SingInInitialState extends SignInState {
   const SingInInitialState();
 }
+
+class SignInLoadingState extends SignInState {
+  const SignInLoadingState();
+}
+
+class SignInSuccessState extends SignInState {
+  const SignInSuccessState();
+}
+
+enum SignInErrorType { generic, internet }
+
+class SignInErrorState extends SignInState {
+  const SignInErrorState(this.type);
+  final SignInErrorType type;
+}
