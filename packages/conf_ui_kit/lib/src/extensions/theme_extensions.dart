c109d2725e6bab1cc7d93a71fd9ec16667c43e88
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-07 11:00:01 -0500

    ♻️ (theme_extensions.dart): refactor ThemeX extension to improve readability and maintainability by using the theme property directly 📝 (theme_extensions.dart): add new color-related getters to ThemeX extension for continue button, selected photo, and flutter navy colors

--- packages/conf_ui_kit/lib/src/extensions/theme_extensions.dart
+++ packages/conf_ui_kit/lib/src/extensions/theme_extensions.dart
@@ -3,7 +3,9 @@ import 'package:conf_ui_kit/src/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 extension ThemeX on BuildContext {
-  TextTheme get textTheme => Theme.of(this).textTheme;
+  ThemeData get theme => Theme.of(this);
+
+  TextTheme get textTheme => theme.textTheme;
 
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
 
@@ -37,4 +39,16 @@ extension ThemeX on BuildContext {
     true => AppColors.onPrimaryDark,
     false => AppColors.onPrimary,
   };
+
+  Color get continueButtonColorEnable => AppColors.onPrimary;
+  Color get continueButtonColorDisable => switch (dark) {
+    true => AppColors.onPrimary.withValues(alpha: .25),
+    false => AppColors.neutralGray,
+  };
+
+  Color get updateSelectedPhotoColor => switch (dark) {
+    true => AppColors.flutterSky,
+    false => AppColors.flutterBlue,
+  };
+  Color get flutterNavy => AppColors.flutterNavy;
 }
