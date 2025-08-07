3f4142da6a1f045736519226624bde37be7ce972
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-07 12:22:09 -0500

    ✨ (auth_page.dart): Add monitorRepository to SignInCubit constructor to support monitoring functionality 🔧 (auth_page.dart): Import flutter/services.dart to use SystemUiOverlayStyle in AppBar ♻️ (auth_page.dart): Refactor AuthButtons widget to use BlocListener for better state management and readability

--- lib/auth/auth_page.dart
+++ lib/auth/auth_page.dart
@@ -1,5 +1,7 @@
+import 'package:conf_shared_models/conf_shared_models.dart';
 import 'package:conf_ui_kit/conf_ui_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_conf_latam/auth/auth.dart';
 import 'package:flutter_conf_latam/common/common.dart';
@@ -25,18 +27,34 @@ class AuthPage extends StatelessWidget {
             return SignInCubit(
               const SingInInitialState(),
               authRepository: context.read(),
+              monitorRepository: context.read(),
             );
           },
         ),
       ],
-      child: const Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: UiConstants.spacing32),
-            child: CustomScrollView(
-              slivers: [AuthHeader(), AuthButtons(), TermsAndConditions()],
+      child: Scaffold(
+        body: Stack(
+          children: [
+            AppBar(
+              backgroundColor: context.scaffoldBackgroundColor,
+              elevation: 0,
+              forceMaterialTransparency: true,
+              systemOverlayStyle: switch (context.dark) {
+                true => SystemUiOverlayStyle.light,
+                false => SystemUiOverlayStyle.dark,
+              },
             ),
-          ),
+            const SafeArea(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: UiConstants.spacing32,
+                ),
+                child: CustomScrollView(
+                  slivers: [AuthHeader(), AuthButtons(), TermsAndConditions()],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -46,6 +64,23 @@ class AuthPage extends StatelessWidget {
 class AuthButtons extends StatelessWidget {
   const AuthButtons({super.key});
 
+  Future<void> _listener(BuildContext context, SignInState state) async {
+    final retry = await context.pushNamed<bool?>(
+      FCLRoutes.authError.name,
+      extra: {'type': state.type?.name},
+    );
+    if (retry != null && retry && context.mounted && state.signInType != null) {
+      return _startSignInFlow(state.signInType!, context);
+    }
+  }
+
+  bool _listenWhen(SignInState previous, SignInState current) {
+    return switch ((previous, current)) {
+      (SignInLoadingState(), SignInErrorState()) => true,
+      (_, _) => false,
+    };
+  }
+
   Future<void> _startSignInFlow(SignInType type, BuildContext context) async {
     final state = context.read<ValueCubit<TermsAcceptanceStatus?>>().state;
     final accepted = state == TermsAcceptanceStatus.accepted;
@@ -54,14 +89,14 @@ class AuthButtons extends StatelessWidget {
     }
     await showLoader(
       context,
-      FCLRoutes.auth.path.replaceAll('/', ''),
+      routeParam: FCLRoutes.auth.path.replaceAll('/', ''),
       _performSignIn(type, context),
     );
   }
 
   Future<void> _performSignIn(SignInType type, BuildContext context) async {
     final result = await context.read<SignInCubit>().signIn(type);
-    if (!context.mounted || !result) return;
+    if (!context.mounted || result is Failure) return;
     return context.read<SessionCubit>().checkSession();
   }
 
@@ -77,20 +112,26 @@ class AuthButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
-      child: Column(
-        children: [
-          ContinueWithApple(
-            onPressed: (type) => _startSignInFlow(type, context),
-          ),
-          const ExcludeSemantics(
-            child: SizedBox(height: UiConstants.spacing12),
-          ),
-          ContinueWithGoogle(
-            onPressed: (type) => _startSignInFlow(type, context),
-          ),
-          const Or(),
-          ContinueAsGuest(onPressed: (type) => _startSignInFlow(type, context)),
-        ],
+      child: BlocListener<SignInCubit, SignInState>(
+        listener: _listener,
+        listenWhen: _listenWhen,
+        child: Column(
+          children: [
+            ContinueWithApple(
+              onPressed: (type) => _startSignInFlow(type, context),
+            ),
+            const ExcludeSemantics(
+              child: SizedBox(height: UiConstants.spacing12),
+            ),
+            ContinueWithGoogle(
+              onPressed: (type) => _startSignInFlow(type, context),
+            ),
+            const Or(),
+            ContinueAsGuest(
+              onPressed: (type) => _startSignInFlow(type, context),
+            ),
+          ],
+        ),
       ),
     );
   }
