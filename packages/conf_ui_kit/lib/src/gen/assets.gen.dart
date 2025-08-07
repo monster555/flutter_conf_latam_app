a7f1e19c9555159bd3618e5f0e60debd241f5c16
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-07 10:59:51 -0500

    ✨ (assets.gen.dart): Add new images 'dashEcuador', 'person', 'quitoBlue', 'quitoWhite' to the assets library for use in the UI kit 📝 (fonts.gen.dart): Generate font family constants for 'Recoleta' to be used in the UI kit 🔧 (gen.dart): Export 'fonts.gen.dart' to include font family constants in the generated assets for the UI kit

--- packages/conf_ui_kit/lib/src/gen/assets.gen.dart
+++ packages/conf_ui_kit/lib/src/gen/assets.gen.dart
@@ -15,8 +15,12 @@ import 'package:vector_graphics/vector_graphics.dart' as _vg;
 class $AssetsBrandGen {
   const $AssetsBrandGen();
 
-  /// File path: assets/brand/dash_sad.svg
-  SvgGenImage get dashSad => const SvgGenImage('assets/brand/dash_sad.svg');
+  /// File path: assets/brand/dash_ecuador.png
+  AssetGenImage get dashEcuador =>
+      const AssetGenImage('assets/brand/dash_ecuador.png');
+
+  /// File path: assets/brand/dash_sad.png
+  AssetGenImage get dashSad => const AssetGenImage('assets/brand/dash_sad.png');
 
   /// File path: assets/brand/fcl_dark.svg
   SvgGenImage get fclDark => const SvgGenImage('assets/brand/fcl_dark.svg');
@@ -25,7 +29,7 @@ class $AssetsBrandGen {
   SvgGenImage get fclLight => const SvgGenImage('assets/brand/fcl_light.svg');
 
   /// List of all assets
-  List<SvgGenImage> get values => [dashSad, fclDark, fclLight];
+  List<dynamic> get values => [dashEcuador, dashSad, fclDark, fclLight];
 }
 
 class $AssetsIconsGen {
@@ -42,20 +46,31 @@ class $AssetsIconsGen {
   SvgGenImage get flutterConfLogoLight =>
       const SvgGenImage('assets/icons/flutter_conf_logo_light.svg');
 
+  /// File path: assets/icons/person.svg
+  SvgGenImage get person => const SvgGenImage('assets/icons/person.svg');
+
   /// List of all assets
   List<SvgGenImage> get values =>
-      [face, flutterConfLogoDark, flutterConfLogoLight];
+      [face, flutterConfLogoDark, flutterConfLogoLight, person];
 }
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
+
+  /// File path: assets/images/quito_blue.svg
+  SvgGenImage get quitoBlue =>
+      const SvgGenImage('assets/images/quito_blue.svg');
+
+  /// File path: assets/images/quito_white.svg
+  SvgGenImage get quitoWhite =>
+      const SvgGenImage('assets/images/quito_white.svg');
 
   /// File path: assets/images/waving_dash.svg
   SvgGenImage get wavingDash =>
       const SvgGenImage('assets/images/waving_dash.svg');
 
   /// List of all assets
-  List<SvgGenImage> get values => [wavingDash];
+  List<SvgGenImage> get values => [quitoBlue, quitoWhite, wavingDash];
 }
 
 class $AssetsSocialIconsGen {
@@ -102,6 +117,91 @@ class Assets {
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsSocialIconsGen socialIcons = $AssetsSocialIconsGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  static const String package = 'conf_ui_kit';
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/conf_ui_kit/$_assetName';
 }
 
 class SvgGenImage {
