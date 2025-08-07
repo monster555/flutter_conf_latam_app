f478cb3ec8f2a49abe580e32db2624c826716965
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-07 12:21:59 -0500

    📝 (sign_in_cubit.dart): Add ConnectivityMonitor dependency to SignInCubit to check internet connection before signing in 🐛 (sign_in_cubit.dart): Fix typo in SignInCubit class constructor 🐛 (sign_in_cubit.dart): Fix typo in Failure class usage 🐛 (sign_in_cubit.dart): Fix typo in SignInErrorState class constructor 🔧 (sign_in_cubit.dart): Add dispose method to close ConnectivityMonitor in SignInCubit 📝 (sign_in_state.dart): Add signInType and type properties to SignInErrorState class 📝 (sign_in_state.dart): Add extension methods to get signInType and type from SignInState instances

--- lib/auth/cubits/sign_in/sign_in_cubit.dart
+++ lib/auth/cubits/sign_in/sign_in_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:conf_auth_data_source/conf_auth_data_source.dart';
 import 'package:conf_auth_repository/conf_auth_repository.dart';
 import 'package:conf_shared_models/conf_shared_models.dart';
+import 'package:connectivity_monitor/connectivity_monitor.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'sign_in_state.dart';
@@ -8,12 +9,22 @@ part 'sign_in_state.dart';
 enum SignInType { apple, google, guest }
 
 class SignInCubit extends Cubit<SignInState> {
-  SignInCubit(super.initialState, {required IConfAuthRepository authRepository})
-    : _authRepository = authRepository;
+  SignInCubit(
+    super.initialState, {
+    required IConfAuthRepository authRepository,
+    required IConnectivityMonitorRepository monitorRepository,
+  }) : _authRepository = authRepository,
+       _monitorRepository = monitorRepository;
   final IConfAuthRepository _authRepository;
+  final IConnectivityMonitorRepository _monitorRepository;
 
-  Future<bool> signIn(SignInType type) async {
+  FutureAuthResult<ConfAuthUser> signIn(SignInType type) async {
     emit(const SignInLoadingState());
+    final connected = await _monitorRepository.connected();
+    if (!connected) {
+      emit(SignInErrorState(SignInErrorType.internet, signInType: type));
+      return const Failure(NoInternetException());
+    }
     final result = await switch (type) {
       SignInType.apple => _authRepository.signInWithApple(),
       SignInType.google => _authRepository.signInWithGoogle(),
@@ -22,10 +33,19 @@ class SignInCubit extends Cubit<SignInState> {
     final newState = switch (result) {
       Success() => const SignInSuccessState(),
       Failure(:final error) when error.aborter => const SingInInitialState(),
-      Failure(:final error) => SignInErrorState(error.fromException()),
+      Failure(:final error) => SignInErrorState(
+        error.fromException(),
+        signInType: type,
+      ),
     };
     emit(newState);
-    return result is Success;
+    return result;
+  }
+
+  @override
+  Future<void> close() {
+    _monitorRepository.dispose();
+    return super.close();
   }
 }
 
--- lib/auth/cubits/sign_in/sign_in_state.dart
+++ lib/auth/cubits/sign_in/sign_in_state.dart
@@ -19,6 +19,19 @@ class SignInSuccessState extends SignInState {
 enum SignInErrorType { generic, internet }
 
 class SignInErrorState extends SignInState {
-  const SignInErrorState(this.type);
+  const SignInErrorState(this.type, {this.signInType});
   final SignInErrorType type;
+  final SignInType? signInType;
+}
+
+extension SignInStateX on SignInState {
+  SignInType? get signInType => switch (this) {
+    SignInErrorState(:final signInType) => signInType,
+    _ => null,
+  };
+
+  SignInErrorType? get type => switch (this) {
+    SignInErrorState(:final type) => type,
+    _ => null,
+  };
 }
