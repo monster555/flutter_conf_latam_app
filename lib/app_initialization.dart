523e50168a193ac710938eafdb65bb62b7efaeee
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-08-07 12:22:46 -0500

    ✨ (app_initialization.dart): introduce new UserRepository and ConnectivityMonitorRepository to handle user data and monitor network connectivity respectively

--- lib/app_initialization.dart
+++ lib/app_initialization.dart
@@ -2,16 +2,20 @@ import 'package:conf_auth_data_source/conf_auth_data_source.dart';
 import 'package:conf_auth_repository/conf_auth_repository.dart';
 import 'package:conf_cache/conf_cache.dart';
 import 'package:conf_cloud_functions_data_source/conf_cloud_functions_data_source.dart';
+import 'package:connectivity_monitor/connectivity_monitor.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_conf_latam/firebase_options.dart';
 import 'package:speakers_repository/speakers_repository.dart';
 import 'package:sponsors_repository/sponsors_repository.dart';
+import 'package:user_repository/user_repository.dart';
 
 typedef AppDependencies =
     ({
       SpeakersRepository speakersRepository,
       SponsorsRepository sponsorsRepository,
       IConfAuthRepository confAuthRepository,
+      IUserRepository userRepository,
+      IConnectivityMonitorRepository connectivityMonitorRepository,
     });
 
 Future<AppDependencies> initializeApp() async {
@@ -26,12 +30,17 @@ Future<AppDependencies> initializeApp() async {
     dataSource: dataSource,
     cache: caches.speakers,
   );
+
+  final connectivityMonitorRepository = ConnectivityMonitorRepository();
+
   final sponsorsRepository = SponsorsRepository(dataSource: dataSource);
   final confAuthRepository = ConfAuthRepository(authDataSource: authDataSource);
-
+  final userRepository = UserRepository(dataSource: dataSource);
   return (
     speakersRepository: speakersRepository,
     sponsorsRepository: sponsorsRepository,
     confAuthRepository: confAuthRepository,
+    userRepository: userRepository,
+    connectivityMonitorRepository: connectivityMonitorRepository,
   );
 }
